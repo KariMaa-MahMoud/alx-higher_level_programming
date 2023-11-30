fab655f5a568b6a2a705f75688856ea061d7d3cd
KariMaa-MahMoud <karima.mahmoud10@gmail.com>
2023-11-30 15:39:58 -0800

    task 2

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# adding function
+def add(a, b):
+    return (a + b)
+
+# substruction function
+def sub(a, b):
+    return (a - b)
+
+# multiplication function
+def mul(a, b):
+    return (a * b)
+
+# division function
+def div(a, b):
+    return int(a / b)
