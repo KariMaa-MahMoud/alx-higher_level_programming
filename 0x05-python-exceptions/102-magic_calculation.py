0df807e87a6727eee1544ad789b432ac5ebcadff
KariMaa-MahMoud <128936629+KariMaa-MahMoud@users.noreply.github.com>
2023-12-21 00:30:43 +0200

    Update 102-magic_calculation.py

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""magic fun"""
+"""Magic calculation def"""
 
 
 def magic_calculation(a, b):
@@ -9,7 +9,7 @@ def magic_calculation(a, b):
             if i > a:
                 raise Exception('Too far')
             else:
-                result += a ** b / i
+                result += (a ** b) / i
         except:
             result = b + a
             break
