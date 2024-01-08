2f19740c3aaa12753fc98e0791ae6459302bbe91
KariMaa-MahMoud <128936629+KariMaa-MahMoud@users.noreply.github.com>
2024-01-08 22:23:09 +0200

    Update 2-matrix_divided.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,57 +1,48 @@
 #!/usr/bin/python3
 """
-
 This module is composed by a function that divides the numbers of a matrix
-
 """
-
 
 
 def matrix_divided(matrix, div):
     """ Function that divides the integer/float numbers of a matrix
-
     Args:
         matrix: list of a lists of integers/floats
         div: number which divides the matrix
-
     Returns:
         A new matrix with the result of the division
-
     Raises:
         TypeError: If the elements of the matrix aren't lists
                    If the elemetns of the lists aren't integers/floats
                    If div is not an integer/float number
                    If the lists of the matrix don't have the same size
-
         ZeroDivisionError: If div is zero
-
-
     """
 
-    if not type(div) in (int, float):
+    if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
 
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    msg_matrixType = "matrix must be a matrix (list of lists) of integers/floats"
+    typerr_msg = "matrix must be a matrix (list of lists) of integers/floats"
 
     if not matrix or not isinstance(matrix, list):
-        raise TypeError(msg_matrixType)
+        raise TypeError(typerr_msg)
 
     len_e = 0
-    msg_matrixSize = "Each row of the matrix must have the same size"
+    msg_size = "Each row of the matrix must have the same size"
 
     for elems in matrix:
         if not elems or not isinstance(elems, list):
-            raise TypeError(msg_matrixType)
+            raise TypeError(typerr_msg)
 
         if len_e != 0 and len(elems) != len_e:
-            raise TypeError(msg_matrixSize)
+            raise TypeError(msg_size)
 
         for num in elems:
-            if not type(num) in (int, float):
-                raise TypeError(msg_matrixType)
+            if not isinstance(num, (int, float)):
+                raise TypeError(typerr_msg)
 
         len_e = len(elems)
 
