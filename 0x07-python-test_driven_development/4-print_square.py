5098da0e75987e563b7e6cedf001e3b90ba7c2c7
KariMaa-MahMoud <128936629+KariMaa-MahMoud@users.noreply.github.com>
2024-01-08 22:24:06 +0200

    Update 4-print_square.py

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -28,7 +28,3 @@ def print_square(size):
 
     for i in range(size):
         print("#" * size)
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testfile.("tests/4-print_square.text")
