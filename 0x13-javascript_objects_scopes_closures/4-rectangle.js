64e6356ed9095ddeb0321d69b39ede75dd456efa
KariMaa-MahMoud <karima.mahmoud10@gmail.com>
2024-03-12 11:46:29 -0700

    task 4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -7,7 +7,10 @@ class Rectangle {
     }
   }
 
-
+  /**
+   * @property {method} print - prints the rectangle using the character X
+   * @returns void
+   */
   print () {
     for (let i = 0; i < this.height; i++) {
       let s = '';
@@ -18,14 +21,20 @@ class Rectangle {
     }
   }
 
-
+  /**
+   * @property {method} rotate - exchanges the width and the height of the rectangle
+   * @returns void
+   */
   rotate () {
     const temp = this.width;
     this.width = this.height;
     this.height = temp;
   }
 
-
+  /**
+   * @property {method} double - multiples the width and the height of the rectangle by 2
+   * @returns void
+   */
   double () {
     this.width *= 2;
     this.height *= 2;
