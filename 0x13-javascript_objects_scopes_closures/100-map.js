1fe1d444cbf14bfdfdd0b5a9a3616faa62296b01
KariMaa-MahMoud <karima.mahmoud10@gmail.com>
2024-03-13 15:12:04 -0700

    fix errros

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,15 +1,5 @@
-<<<<<<< HEAD
 #!/usr/bin/node
 const initList = require('./100-data.js').list;
 const newList = initList.map((number, index) => number * index);
-
 console.log(initList);
 console.log(newList);
-=======
-#!/usr/bin/node
-const List = require('./100-data.js').list;
-const newList = List.map((number, index) => number * index);
-
-console.log(List);
-console.log(newList);
->>>>>>> 033f9dd759c67b1a6835f9a8f0346250603c1538
