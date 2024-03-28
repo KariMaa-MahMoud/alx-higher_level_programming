36db2a9a6b669dde0354d4f455e8db1c37735dd4
KariMaa-MahMoud <128936629+KariMaa-MahMoud@users.noreply.github.com>
2024-03-28 02:18:48 +0200

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,18 +2,19 @@
 """ Finds Peak values """
 
 def find_peak(list_of_integers):
+    """Find the peak"""
     if len(list_of_integers) == 0:
         return None
     
-    low = 0
-    high = len(list_of_integers) - 1
-    
-    while low < high:
-        mid = (low + high) // 2
-        
-        if list_of_integers[mid] < list_of_integers[mid + 1]:
-            low = mid + 1
-        else:
-            high = mid
-    
-    return list_of_integers[low]
+""" binary search algorithim """
+
+
+def binary_search(a, lo, hi):
+    """Recursive binary search of the peak"""
+    if lo >= hi:
+        return lo
+    mid = ((hi - lo) // 2) + lo
+    if a[mid] > a[mid + 1]:
+        return binary_search(a, lo, mid)
+    else:
+        return binary_search(a, mid + 1, hi)
